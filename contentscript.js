17f81f2058018fd8236855adcf3138ab81723af8
bmburhan22 <83741890+bmburhan22@users.noreply.github.com>
2021-07-05 20:16:23 +0300

    Update contentscript.js

--- contentscript.js
+++ contentscript.js
@@ -1,5 +1,5 @@
 let e = document.getElementById('ember29').parentElement;
-iconpath = chrome.extension.getURL('icons/notifications.png');
+iconpath = chrome.extension.getURL('notifications.png');
 e.innerHTML+=`<img id="popupnotifications" src="${iconpath}" width="84" height="84"></img>`;
 document.getElementById("popupnotifications").onclick = function(){
     let newWindow = window.open("https://www.linkedin.com/notifications/", "Notifications", "width=400,height=500,left=1000,top=40");
