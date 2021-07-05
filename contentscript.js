f14412901028c0bd4b70ea8c345f1e3301cd5609
bmburhan22 <83741890+bmburhan22@users.noreply.github.com>
2021-07-05 20:14:47 +0300

    Add files via upload

---
+++ contentscript.js
@@ -0,0 +1,9 @@
+let e = document.getElementById('ember29').parentElement;
+iconpath = chrome.extension.getURL('icons/notifications.png');
+e.innerHTML+=`<img id="popupnotifications" src="${iconpath}" width="84" height="84"></img>`;
+document.getElementById("popupnotifications").onclick = function(){
+    let newWindow = window.open("https://www.linkedin.com/notifications/", "Notifications", "width=400,height=500,left=1000,top=40");
+    newWindow.onload = function() {
+    var d = newWindow.document.getElementById('main');
+    newWindow.document.body.innerHTML = ''; 
+    newWindow.document.body.append(d); }}
